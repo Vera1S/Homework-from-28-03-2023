d1c5f9b87175e7e74920a867308d09d2ce0d299b
Vera1S <128926912+Vera1S@users.noreply.github.com>
2023-04-02 14:42:45 -0400

    Add files via upload
    Условия задачи: Создайте класс, стартовую точку,
     где через Scanner вы будете получать целое число и 
    трансформировать его в двоичную систему (выводить на экран)

---
+++ HomeworkScanner.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class HomeworkScanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите случайное целое число: 4*Math.pow(10,2) + 3*Math.pow(10,1) + 2*Math.pow(10,0)");
+        int a = sc.nextInt();
+        System.out.print("Введено число " + a);
+
+
+
+
+
+
+
+
+
+
+
+
+        sc.close();
+
+    }
+}
